3d2f7e9aca1ef8d3857525ff3d90b539c97d5e6d
saint-lag <maiasena.gabriel@gmail.com>
2022-04-10 18:24:36 -0300

    [REQ_6][REQ_7][REQ_8][LINT][TESTADO LOCALMENTE]: Finalizados

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      totalExpenses: 0,
-    };
-  }
-
   render() {
-    const { email } = this.props;
-    const { totalExpenses } = this.state;
+    const { email, totalExpenses } = this.props;
     return (
       <header>
         <div id="header-email-field-div">
@@ -21,7 +13,9 @@ class Header extends React.Component {
         </div>
         <div id="header-total-field-div">
           <span />
-          <span data-testid="total-field">{totalExpenses}</span>
+          <span data-testid="total-field">
+            {totalExpenses ? totalExpenses.toFixed(2) : 0}
+          </span>
           <span data-testid="header-currency-field">BRL</span>
         </div>
       </header>
@@ -30,23 +24,13 @@ class Header extends React.Component {
 }
 
 Header.propTypes = {
-  email: PropTypes.string.isRequired,
-  // expenses: PropTypes.string.isRequired,
-};
+  email: PropTypes.string,
+  totalExpenses: PropTypes.string,
+}.isRequired;
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
-  // expenses: state.wallet.expenses,
+  totalExpenses: state.wallet.totalExpenses,
 });
 
 export default connect(mapStateToProps)(Header);
-
-// {
-//   user: {
-//     email: '',
-//   },
-//   wallet: {
-//     currencies: [],
-//     expenses: []
-//   }
-// }
